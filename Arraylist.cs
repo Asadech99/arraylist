9c33e4e76e77d7573a1063be75a44cff6f14f46b
Asadech99 <65153776+Asadech99@users.noreply.github.com>
2020-05-12 11:55:56 +0700

    Initial commit

---
+++ Arraylist.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+
+namespace arraylist
+{
+    internal class Arraylist : ArrayList
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace arraylist
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arraylist;
+        private object arrayList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            arraylist = new Arraylist();
+
+        }
+
+        private void show_Click(object sender, RoutedEventArgs e)
+        {
+            //show data
+
+            
+            foreach (string data in arraylist)
+            {
+
+                MessageBox.Show("Count : " + arraylist.Count.ToString() + "Data : " + data);
+            }
+
+        }
+        //add data
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            arraylist.Add(infor.Text);
+            MessageBox.Show("add ข้อมูลสำเร็จ");
+        }
+
+        private void remove_Click(object sender, RoutedEventArgs e)
+        {
+            arraylist.Remove(infor.Text);
+            MessageBox.Show("Remove ข้อมูลสำเร็จ");
+        }
+
+        
+    }
+
+}
